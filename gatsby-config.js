47ea93a5f2d583e62807a8b5cfea950cbae26828
Patrick Muff <muff.patrick@muffag.ch>
2019-06-01 22:28:54 +0200

    refactor: make the site more basic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Patrick Muff - Blog`,
+    description: `Personal blog of Patrick Muff`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
