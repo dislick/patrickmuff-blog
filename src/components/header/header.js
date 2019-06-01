aab43cea790ba197f4b3bfd1b68491915999ed38
Patrick Muff <muff.patrick@muffag.ch>
2019-06-02 00:55:43 +0200

    feat(header): add more details

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,21 +6,25 @@ import styles from './header.module.css';
 import { getAge } from './get_age/get_age';
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div className={styles.header}>
-      <Link to='/' className={styles.authorContainer}>
-        <AuthorImage className={styles.author} />
-      </Link>
-      <div className={styles.about}>
-        <h1 className={styles.title}>{siteTitle}</h1>
-        <h2 className={styles.subtitle}>Blog</h2>
-        <p>
-          {getAge(new Date(1993, 9, 1))} year old web enthusiast and person.{' '}
-          <Link to='/about'>Read more about me</Link>
-        </p>
+  <>
+    <header>
+      <div className={styles.header}>
+        <Link to='/' className={styles.authorContainer}>
+          <AuthorImage className={styles.author} />
+        </Link>
+        <div className={styles.about}>
+          <h1 className={styles.title}>{siteTitle}</h1>
+          <p className={styles.aboutText}>
+            {getAge(new Date(1993, 9, 1))} year old web enthusiast living in
+            Switzerland. Interested in software engineering, design and motor
+            racing.
+          </p>
+          <Link to='/about'>More about me</Link>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+    <hr className={styles.divider} />
+  </>
 );
 
 Header.propTypes = {
