ecae593441844faecf0956945a45d88aaf425de0
Patrick Muff <muff.patrick@muffag.ch>
2019-06-01 22:46:17 +0200

    style: use sans serif font

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
-import Header from './header/header';
+import Header from '../header/header';
 import 'normalize.css';
+import './layout.css';
+import styles from './layout.module.css';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -16,20 +18,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <div className={styles.layout}>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>© {new Date().getFullYear()}</footer>
-        </div>
-      </>
+        <main>{children}</main>
+        <footer>© {new Date().getFullYear()}</footer>
+      </div>
     )}
   />
 );
