0618e2588cff168742b9830164c91e1cc447f183
Patrick Muff <muff.patrick@muffag.ch>
2019-06-21 08:12:35 +0200

    refactor(header): remove <> tags that were not needed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,37 +2,35 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import AuthorImage from '../author_image';
-import styles from './header.module.css';
 import { getAge } from './get_age/get_age';
+import styles from './header.module.css';
 
 const Header = ({ siteTitle }) => (
-  <>
-    <header>
-      <div className={styles.header}>
-        <div className={styles.authorContainer}>
-          <Link to='/'>
-            <AuthorImage className={styles.author} />
-          </Link>
-        </div>
-        <div className={styles.about}>
-          <h1 className={styles.title}>{siteTitle}</h1>
-          <p className={styles.aboutText}>
-            {getAge(new Date(1993, 9, 1))} year old web enthusiast living in
-            Switzerland. Interested in software engineering, design, and motor
-            racing.
-          </p>
-          <ul className={styles.links}>
-            <li>
-              <Link to='/about'>About Me</Link>
-            </li>
-            <li>
-              <Link to='/links'>Link Collection</Link>
-            </li>
-          </ul>
-        </div>
+  <header>
+    <div className={styles.header}>
+      <div className={styles.authorContainer}>
+        <Link to='/'>
+          <AuthorImage className={styles.author} />
+        </Link>
       </div>
-    </header>
-  </>
+      <div className={styles.about}>
+        <h1 className={styles.title}>{siteTitle}</h1>
+        <p className={styles.aboutText}>
+          {getAge(new Date(1993, 9, 1))} year old web enthusiast living in
+          Switzerland. Interested in software engineering, design, and motor
+          racing.
+        </p>
+        <ul className={styles.links}>
+          <li>
+            <Link to='/about'>About Me</Link>
+          </li>
+          <li>
+            <Link to='/links'>Link Collection</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
 );
 
 Header.propTypes = {
